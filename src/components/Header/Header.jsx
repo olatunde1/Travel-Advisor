55e6eead2881a130c437efdca8f6c68e1863b862
Olatunde <idowuolatunde32@gmail.com>
2022-08-17 01:54:46 +0100

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, } from '@material-ui/core'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
